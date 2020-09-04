8da93403060a1138880c96067393bf619a80547d
杨铖 <853364106@qq.com>
2020-09-04 16:09:58 +0800

    Version1.0

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,85 @@
 import React from 'react';
-import { Container, Button } from '@material-ui/core';
+import { Container, Chip, Grid, Button, Divider, TextField, Tooltip } from '@material-ui/core';
+import Axios from 'axios';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 class Index extends React.Component {
-    onClickEvent(t) {
-        alert(t)
+    constructor() {
+        super()
+        this.state = {
+            list: [{ id: 1, content: 'sad', }],
+            input: ''
+        }
+        Axios.get("/api/get_all_clipboard").then((response) => {
+            if (response.data === []) {
+                this.setState({ list: [{ id: 1, content: '暂无数据' }] })
+            } else {
+                this.setState({ list: response.data })
+            }
+        })
+    }
+    onDeleteEvent(index, id) {
+        const list = this.state.list
+        list.splice(index, 1)
+        this.setState({
+            list: list
+        })
+        Axios.get("/api/remove_clipboard", { params: { id: id } }).then(
+            (response) => {
+
+            }
+        )
+        Axios.get("/api/get_all_clipboard").then((response) => {
+            if (response.data === []) {
+                this.setState({ list: [{ id: 1, content: '暂无数据' }] })
+            } else {
+                this.setState({ list: response.data })
+            }
+        })
+    }
+    onClickEvent(v) {
+    }
+    onChangeEvent(e) {
+        this.setState({ input: e.target.value })
+    }
+    add() {
+        Axios.get('/api/add_clipboard', { params: { content: this.state.input } }).then(
+            (response) => {
+                if (response.data) {
+                    this.setState({
+                        open: true
+                    })
+                }
+            }
+        )
+        Axios.get("/api/get_all_clipboard").then((response) => {
+            if (response.data === []) {
+                this.setState({ list: [{ id: 1, content: '暂无数据' }] })
+            } else {
+                this.setState({ list: response.data })
+            }
+        })
     }
     render() {
         return (
             <Container maxWidth='sm'>
-                <Button variant="contained" onClick={() => this.onClickEvent(this.props.title)} color='primary'>{this.props.title}</Button>
+                <Grid container spacing={3} justify='center' alignItems="center">
+                    <Grid item xs={4}>
+                        <TextField value={this.state.input} label='在这里输入' onChange={(e) => { this.onChangeEvent(e) }}></TextField>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant="contained" color='primary' onClick={() => { this.add() }}>添加</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider variant="middle" />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {this.state.list.map((v, index) =>
+                            <CopyToClipboard text={v.content} key={v.id}>
+                                <Chip label={v.content} variant="outlined" onDelete={() => { this.onDeleteEvent(index, v.id) }} onClick={() => { this.onClickEvent(v) }} />
+                            </CopyToClipboard>
+                        )}
+                    </Grid>
+                </Grid>
             </Container>
         );
     }
